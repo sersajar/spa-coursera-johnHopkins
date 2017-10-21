ee2bf32c9031044a256ce28c63ad6f3a4b0ce940
Sergi Sabate <sersajar@gmail.com>
2017-10-21 20:51:05 +0200

    lecture11 expressions

--- app.js
+++ app.js
@@ -1,50 +1,21 @@
-/*(function () {
+(function () {
     'use strict';
 
-    angular.module('DIApp', [])
-        .controller('DIController', DIController);
-        
-    DIController.$inject = ['$scope', '$filter'];
-    function DIController($scope, $filter) {
+    angular.module('MsgApp', [])
+            .controller('MsgController', MsgController);
 
-        $scope.name = "Yaakov";
+            MsgController.$inject = ['$scope'];
+            function MsgController($scope) {
+                $scope.name = "Sergi";
+                $scope.stateOfBeing = "hungry";
 
-        $scope.upper = function () {
-            var upCase = $filter('uppercase');
-            $scope.name = upCase($scope.name);
-        };
-    };
-})();*/
-
-//(function(){
-//    'use strict';
-//// inline injection, hard to read, same functionallity as above
-//    angular.module('DIApp', [])
-//            .controller('DIController', ['$scope', '$filter', function ($scope, $filter) {
-//                
-//                $scope.name = "Yaakov";
-//                
-//                $scope.upper = function () {
-//                    var upCase = $filter('uppercase');
-//                    $scope.name = upCase($scope.name);
-//                }; 
-//            }]);
-//// same inline injection different written accepts minification
-//
-///*'use strict';
-//    
-//    angular.module('DIApp', [])
-//            .controller('DIController', ['$scope', '$filter', DIController]);
-//                        
-//            function DIController($scope, $filter) {
-//                
-//                $scope.name = "Yaakov";
-//                
-//                $scope.upper = function () {
-//                    var upCase = $filter('uppercase');
-//                    $scope.name = upCase($scope.name);
-//                }; 
-//            };*/
-//})();
-
-!function(){"use strict";angular.module("DIApp",[]).controller("DIController",["$scope","$filter",function(e,n){e.name="Yaakov",e.upper=function(){var o=n("uppercase");e.name=o(e.name)}}])}();
+                $scope.sayMessage = function () {
+                    return "Sergi likes to smoke healthy crack at night";
+                };
+                
+                $scope.smokeSergi = function () {
+                    $scope.stateOfBeing = "fed";
+                };
+            }
+    
+})();
