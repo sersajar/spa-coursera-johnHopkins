e462cb83537840c7ba027af5992e7b9a57b178ba
Sergi Sabate <sersajar@gmail.com>
2017-10-21 02:22:41 +0200

    lecture 5 binding with ng-model

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 (function () {
+'use strict';
     
     angular.module('myFirstApp', [])
             // angular convention uses CamelCase for controller names
-            .controller('MyFirstController', function () {
-        
+            .controller('MyFirstController', function ($scope) {
+                $scope.name = "Sergi";
+                $scope.sayHello = function () {
+                    return "Hello Coursera!"
+                };
             });
     
 })();
