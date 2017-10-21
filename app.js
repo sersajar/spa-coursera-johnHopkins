ff99d3d5046305a04ba948e469c244fd6734b6a9
Sergi Sabate <sersajar@gmail.com>
2017-10-21 18:06:07 +0200

    lecture10 protecting min with inject

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
-(function () {
-'use strict';
+/*(function () {
+    'use strict';
 
     angular.module('DIApp', [])
         .controller('DIController', DIController);
+        
+    DIController.$inject = ['$scope', '$filter'];
+    function DIController($scope, $filter) {
 
-        function DIController ($scope, $filter, $injector) {
+        $scope.name = "Yaakov";
 
-            $scope.name = "Yaakov";
+        $scope.upper = function () {
+            var upCase = $filter('uppercase');
+            $scope.name = upCase($scope.name);
+        };
+    };
+})();*/
 
-            $scope.upper = function () {
-                var upCase = $filter('uppercase');
-                $scope.name = upCase($scope.name);    
-            };
+//(function(){
+//    'use strict';
+//// inline injection, hard to read, same functionallity as above
+//    angular.module('DIApp', [])
+//            .controller('DIController', ['$scope', '$filter', function ($scope, $filter) {
+//                
+//                $scope.name = "Yaakov";
+//                
+//                $scope.upper = function () {
+//                    var upCase = $filter('uppercase');
+//                    $scope.name = upCase($scope.name);
+//                }; 
+//            }]);
+//// same inline injection different written accepts minification
+//
+///*'use strict';
+//    
+//    angular.module('DIApp', [])
+//            .controller('DIController', ['$scope', '$filter', DIController]);
+//                        
+//            function DIController($scope, $filter) {
+//                
+//                $scope.name = "Yaakov";
+//                
+//                $scope.upper = function () {
+//                    var upCase = $filter('uppercase');
+//                    $scope.name = upCase($scope.name);
+//                }; 
+//            };*/
+//})();
 
-            console.log($injector.annotate(DIController));
-        }
-
-    function AnnonateMe(name, job, blah) {
-      return "Blah!";
-    }
-
-    console.log(DIController.toString());
-
-})();
+!function(){"use strict";angular.module("DIApp",[]).controller("DIController",["$scope","$filter",function(e,n){e.name="Yaakov",e.upper=function(){var o=n("uppercase");e.name=o(e.name)}}])}();
