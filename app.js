ccefb8777166618d550064b98623767d889b04cf
Sergi Sabate <sersajar@gmail.com>
2017-10-21 03:01:24 +0200

    lecture 6 num name calculator

--- app.js
+++ app.js
@@ -8,6 +8,27 @@
                 $scope.sayHello = function () {
                     return "Hello Coursera!"
                 };
-            });
+            })
     
+            // this controller is for the calculator
+            .controller('NameCalculatorController', function ($scope) {
+                $scope.name = "";
+                $scope.totalValue = 0;
+
+                $scope.displayNumeric = function () {
+                    var totalNameValue = calculateNumericForString($scope.name);
+                    $scope.totalValue = totalNameValue;
+                };
+
+
+                function calculateNumericForString(string) {
+                    
+                    var totalStringValue = 0;
+                    for (var i = 0; i < string.length; i++) {
+                        totalStringValue += string.charCodeAt(i);
+                    };
+                    
+                    return totalStringValue;
+                };
+            });   
 })();
