9f1ceaf9328d5f43985d95155160d817af7af6c5
Sergi Sabate <sersajar@gmail.com>
2017-10-23 00:47:21 +0200

    filter factories

--- app.js
+++ app.js
@@ -2,23 +2,48 @@
     'use strict';
 
     angular.module('MsgApp', [])
-        .controller('MsgController', MsgController);
+        .controller('MsgController', MsgController)
+        // custom filter factories
+        .filter('loves', LovesFilter)
+        .filter('truth', TruthFilter); // not injected cause only used in html
 
-        MsgController.$inject = ['$scope', '$filter'];
-        function MsgController($scope, $filter) {
-            $scope.name = "Yaakov";
+        MsgController.$inject = ['$scope', '$filter', 'lovesFilter'];
+        function MsgController($scope, $filter, lovesFilter) {
+            $scope.name = "Sergi";
             $scope.stateOfBeing = "hungry";
-            $scope.cookieCost = .45;
+            $scope.cookieCost = 0.45;
 
             $scope.sayMessage = function () {
-                var msg = "Yaakov likes to eat healthy snacks at night!";
+                var msg = $scope.name + " likes to eat healthy snacks at night!";
                 var output = $filter('uppercase')(msg);
                 return output;
+            };
+            
+            $scope.sayLovesMessage = function () {
+                var msg = $filter('uppercase')($scope.name) + " likes to eat healthy snacks at night!";
+                msg = lovesFilter(msg);
+                return msg;
             };
 
         $scope.feedYaakov = function () {
             $scope.stateOfBeing = "fed";
             };
         }
-
+        
+        function LovesFilter() {
+            return function (input) {
+                input = input || "";
+                input = input.replace("likes", "loves");
+                return input;
+            };
+        }
+        
+        function TruthFilter() {
+            return function (input, target, replace) {
+                input = input || "";
+                input = input.replace(target, replace);
+                return input;
+            };
+        }    
+    
 })();
